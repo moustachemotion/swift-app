33260bb735744cb206472adc80a07280de96bf71
Daryl Jacob <daryl@moustachemotion.com>
2017-05-03 00:04:29 -0400

    first clean commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        print(text1)
+        print(text1.text!)
+        print(text2.text!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
